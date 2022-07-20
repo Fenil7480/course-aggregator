a8050a3799220bd67df72eaaaebb41d9b95b7392
Saniya Memon <saniyamemon7862@gmail.com>
2022-07-20 02:58:58 -0400

    Navigation bar completed!

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,14 @@
 const Nav = () => {
   return (
     <>
-      <nav className="px-2 bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+      <nav className="px-2 bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700 shadow-lg ">
         <div className="container flex flex-wrap justify-between items-center mx-auto">
           <a href="/" className="flex items-center">
             <img
-              src="/docs/images/logo.svg"
-              className="mr-3 h-6 sm:h-10"
-              alt="Flowbite Logo"
+              src="logo.png"
+              className="mr-3 h-20"
+              alt="Web 3.0 - A new chapter"
             />
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
-            </span>
           </a>
           <button
             data-collapse-toggle="mobile-menu"
@@ -29,9 +26,9 @@ const Nav = () => {
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               ></path>
             </svg>
           </button>
@@ -40,35 +37,26 @@ const Nav = () => {
               <li>
                 <a
                   href="/"
-                  className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent"
+                  className="font-semibold text-lg block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent "
                   aria-current="page"
                 >
                   HOME
                 </a>
               </li>
-
               <li>
                 <a
                   href="/"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 dark:hover:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+                  className="font-semibold text-lg block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 dark:hover:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
                 >
-                  Services
+                  COURSES
                 </a>
               </li>
               <li>
                 <a
                   href="/"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 dark:hover:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+                  className="font-semibold block text-lg py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 dark:hover:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
                 >
-                  Pricing
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 dark:hover:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
-                >
-                  Contact
+                  FAQS
                 </a>
               </li>
             </ul>
