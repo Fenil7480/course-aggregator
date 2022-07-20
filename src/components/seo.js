1f748713b991ef0548530f0738746ab1e3cd3c46
Fenil <fpatel7480@conestogac.on.ca>
2022-07-20 13:32:25 -0400

    file structure changed & course page link added

---
+++ src/components/seo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from "next/head";
+
+const SEO = () => {
+    return (
+        <>
+            <Head>
+                <title>Web 3.0 | A new chapter</title>
+                <meta name="description" content="A platform to find best courses to learn about different technologies of web3." />
+                <link rel="icon" href="/icon.png" />
+            </Head>
+        </>
+    )
+}
+
+export default SEO
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import Nav from '../components/navbar'
+import Footer from '../components/footer'
+import SEO from '../components/seo'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <SEO />
+    <Nav />
+    <Component {...pageProps} />
+    <Footer />
+  </>
 }
 
 export default MyApp
