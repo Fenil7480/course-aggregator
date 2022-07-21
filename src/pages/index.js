e59f483c9dbe26257893fd04ce38e97fc0c6f818
Fenil <fpatel7480@conestogac.on.ca>
2022-07-21 00:33:06 -0400

    tem. content added to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,35 @@
-import Image from "next/image";
-import Sample from "../components/sample";
+// import logo from './logo.svg';
+// import './App.css';
+// import Image from "next/image";
+// import Sample from "../components/sample";
 import styles from "../styles/Home.module.css";
+// import React, { Component } from "react";
+import CardlistComponent from '../components/CardListcomponent';
+import MainfeauturesComponent from '../components/Mainfeautures';
+import AboutPartHome from '../components/AboutPartHome';
+// import Carousal from '../components/Carousal';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      {
-        <h1 className="text-3xl font-bold underline">
-          Hello, This is next js boilerplate structure with tailwind
-        </h1>
-      }
-      <Sample />
+      <div className="App">
+        {/* <Carousal /> */}
+
+        {/*Features part start*/}
+        <MainfeauturesComponent />
+        {/*Features part*/}
+
+        {/*About card view start*/}
+        <AboutPartHome />
+        {/*About card view end*/}
+
+
+        {/*Card for courses start*/}
+        <CardlistComponent />
+        {/*Card for courses end*/}
+
+      </div>
     </div>
   );
 }
+
