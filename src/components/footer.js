35cb936fa91573fd13549ec59251d52dc9a148f5
Saniya Memon <saniyamemon7862@gmail.com>
2022-07-21 00:13:25 -0400

    now footer is responsive!

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,11 @@
 const Footer = () => {
   return (
     <>
-      <footer className="p-4 bg-white sm:p-6 dark:bg-gray-900 flex flex-wrap justify-between items-center mx-auto">
-        <div className="">
-          <p className="customFont font-normal text-lg block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-gray-700 md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent ">
-            &copy; WEB3.0 2022
-          </p>
-        </div>
+      <footer className="p-4 bg-white mx-auto flex flex-wrap justify-center md:justify-between ">
+        <p className="customFont font-normal text-lg block py-2 pr-4 pl-3 text-white rounded text-gray-700 md:p-0 sm:text-center  ">
+          &copy; WEB3.0 2022
+        </p>
+
         <div className="flex flex-wrap items-center px-3">
           <a href="https://www.facebook.com/" target="_blank">
             <img src="f.png" className="h-10 ml-2" />
