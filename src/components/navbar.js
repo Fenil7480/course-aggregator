035cd8e431c49aa6e6ca3d42a5f421f6ec285ea2
annetawamonosl <anneta@studiolocale.com>
2022-07-21 21:34:59 -0400

    Feat: Added FAQ page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -54,10 +54,8 @@ const Nav = () => {
                 </Link>
               </li>
               <li>
-                <Link href="#">
-                  <a
-                    href="courses"
-                    className="font-semibold block text-lg py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 dark:hover:text-white focus:text-blue-700 dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+                <Link href="/faq">
+                  <a className="font-semibold block text-lg py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 dark:hover:text-white focus:text-blue-700 dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
                   >
                     FAQS
                   </a>
