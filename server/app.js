99892b12041fcc432bf801d48ca36025c6125ec4
Igajendran <88035953+Igajendran@users.noreply.github.com>
2022-07-26 21:32:44 -0400

    Api_Endpoints

--- server/app.js
+++ server/app.js
@@ -8,6 +8,8 @@ var mongoose = require("mongoose");
 const dbURI =
   "mongodb+srv://admin:admin@saniyacluster.xrzit.mongodb.net/CourseDB?retryWrites=true&w=majority";
 mongoose.connect(dbURI, { dbName: "CourseDB" });
+//ishu added
+require('./models/coursesSchema');
 
 // CONNECTION EVENTS
 mongoose.connection.on("connected", function () {
@@ -17,4 +19,13 @@ mongoose.connection.on("error", function (err) {
   console.log("Mongoose connection error: " + err);
 });
 
-app.listen(3000, console.log("Server started"));
+//Ishu added
+const apiRouter= require('./routes/index');
+app.use('/api', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,PUT');
+  next();
+ });
+ app.use('/api', apiRouter);
+app.listen(4000, console.log("Server started"));
