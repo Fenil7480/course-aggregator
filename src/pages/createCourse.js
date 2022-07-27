e580e232de527dc80dea3ea1b8578e3a67a4bf89
Saniya Memon <saniyamemon7862@gmail.com>
2022-07-27 11:31:17 -0400

    Admin cerate course file created

---
+++ src/pages/createCourse.js
@@ -0,0 +1,9 @@
+import styles from "../styles/Home.module.css";
+import FeaturedCourses from "../components/featuredCourses";
+import Banner from "../components/banner";
+
+export default createCourse(){
+    return (<>
+        
+    </>);
+}
