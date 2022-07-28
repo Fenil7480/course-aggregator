dcdc0fc118184fb35200eb8ad1e4e4536c09be32
Saniya Memon <saniyamemon7862@gmail.com>
2022-07-28 12:57:17 -0400

    AddCourse component design modified!

--- src/components/adminCourseAdd.js
+++ src/components/adminCourseAdd.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const AdminCourseAdd = ({ name, placeHolder }) => {
-    return (
-        <div className='flex flex-col justify-center'>
-            <label htmlFor="title">{name}</label>
-            <input className='border border-gray-400 py-2 px-5 mb-5 mt-2 rounded-md' type="text" id='title' placeholder={placeHolder} />
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col justify-center ">
+      <label htmlFor="title" className="font-sans font-bold px-5 py-1">
+        {name}
+      </label>
+      <input
+        className="border border-gray-400 py-2 px-5 mb-5 mt-2 rounded-none font-sans"
+        type="text"
+        id="title"
+        placeholder={placeHolder}
+      />
+    </div>
+  );
+};
 
-export default AdminCourseAdd
+export default AdminCourseAdd;
