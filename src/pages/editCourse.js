3b9e915dbb72ae5738c92efcc16864949393cd0b
Saniya Memon <saniyamemon7862@gmail.com>
2022-07-28 12:56:09 -0400

    Admin Create course page design complete!

--- src/pages/editCourse.js
+++ src/pages/editCourse.js
@@ -1,19 +1,30 @@
-import React from 'react'
-import AdminCourseAdd from '../components/adminCourseAdd'
+import React from "react";
+import AdminCourseAdd from "../components/adminCourseAdd";
 
 const EditCourse = () => {
-    return (
-        <div className="flex mt-32 flex-col justify-center w-3/6 m-auto">
-            <AdminCourseAdd name='Title' placeHolder="Title of the course" />
-            <AdminCourseAdd name='Author' placeHolder="Author of the course" />
-            <AdminCourseAdd name='Price' placeHolder="Price for the course" />
-            <AdminCourseAdd name='Technology' placeHolder="Technologies this course covers" />
-            <AdminCourseAdd name='Description' placeHolder="Description about the course" />
-            <AdminCourseAdd name='URL' placeHolder="URL of the source" />
-            <AdminCourseAdd name='Rating' placeHolder="Rating for this course" />
-            <button className='bg-blue-500 rounded-md px-5 py-2 mt-5 w-fit mb-24' type="submit">Add Course</button>
-        </div>
-    )
-}
+  return (
+    <div className="flex mt-32 flex-col justify-center w-3/6 m-auto">
+      <AdminCourseAdd name="Title" placeHolder="Title of the course" />
+      <AdminCourseAdd name="Author" placeHolder="Author of the course" />
+      <AdminCourseAdd name="Price" placeHolder="Price for the course" />
+      <AdminCourseAdd
+        name="Technology"
+        placeHolder="Technologies this course covers"
+      />
+      <AdminCourseAdd
+        name="Description"
+        placeHolder="Description about the course"
+      />
+      <AdminCourseAdd name="URL" placeHolder="URL of the source" />
+      <AdminCourseAdd name="Rating" placeHolder="Rating for this course" />
+      <button
+        className="bg-blue-500 rounded-md px-5 py-2 mt-5 w-fit mb-24"
+        type="submit"
+      >
+        Add New Course
+      </button>
+    </div>
+  );
+};
 
-export default EditCourse
+export default EditCourse;
