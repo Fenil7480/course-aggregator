55e1445b30c310fd9217fc0f8053f057b0d13471
annetawamonosl <anneta@studiolocale.com>
2022-08-09 17:37:37 -0400

    Fix: missing BG image on details page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import Container from "./container";
 
 const Header = ({blue, children}) => {
     
     return (
-        <header className={`${blue ? "bg-blue-800 text-white-100" : "bg-white-100 text-black"} container relative mx-auto px-8 lg:px-16 text-center py-24`}>
-            {children}
+        <header className={`${blue ? "bg-blue-800 text-white-100" : "bg-white-100 text-black"} relative px-8 lg:px-16 text-center py-24`}>
+            <Container>
+                {children}
+            </Container>
             <div className={`headerDivider w-4/12 h-16 rounded-tl-full absolute right-0 bottom-0 ${blue ? "bg-white-100" : "bg-blue-800"}`}></div>
-            <img className='absolute bottom-0 right-6 h-32 w-auto mobilehide' src='interface/squares2.svg' />
+            <img className='absolute bottom-0 right-6 h-32 w-auto mobilehide' src='../interface/squares2.svg' />
         </header>
     )
 }
