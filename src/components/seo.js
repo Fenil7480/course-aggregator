c7f9e92e56e0e771ebf03147964a74dbb6b87910
Fenil7480 <fpatel7480@conestogac.on.ca>
2022-08-17 18:35:55 -0400

    seo tags added

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React from "react";
 import Head from "next/head";
 
 const SEO = () => {
-    return (
-        <>
-            <Head>
-                <title>Web 3.0 | A new chapter</title>
-                <meta name="description" content="A platform to find best courses to learn about different technologies of web3." />
-                <link rel="icon" href="/icon.png" />
-            </Head>
-        </>
-    )
-}
+  return (
+    <>
+      <Head>
+        <title>Web 3.0 | A new chapter</title>
+        <meta
+          name="description"
+          content="A platform to find best courses to learn about different technologies of web3."
+        />
+        <link rel="icon" href="/icon.png" />
+        <meta name="robots" content="index, follow" />
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+      </Head>
+    </>
+  );
+};
 
-export default SEO
+export default SEO;
